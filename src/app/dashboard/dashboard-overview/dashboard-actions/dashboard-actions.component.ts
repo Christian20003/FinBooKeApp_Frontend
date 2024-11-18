18e3eb67d3b379e5d3ffde978a22002e75efff9f
Christian20003 <christian.goellner@bnv-bamberg.de>
2024-11-18 18:13:02 +0100

    feat: Add dummy structure to the dashboard path

---
+++ src/app/dashboard/dashboard-overview/dashboard-actions/dashboard-actions.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-actions',
+  templateUrl: './dashboard-actions.component.html',
+  styleUrl: './dashboard-actions.component.scss',
+})
+export class DashboardActionsComponent {}
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardOverviewComponent } from './dashboard-overview/dashboard-overview.component';
 import { SharedModule } from '../shared/components/shared.module';
+import { DashboardActionsComponent } from './dashboard-overview/dashboard-actions/dashboard-actions.component';
 
 @NgModule({
-  declarations: [DashboardOverviewComponent],
+  declarations: [DashboardOverviewComponent, DashboardActionsComponent],
   imports: [CommonModule, SharedModule],
   exports: [DashboardOverviewComponent],
 })
